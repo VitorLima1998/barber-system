2fe2da6be63c9d1bb4710923a0fffdce434f452f
VitorLima1998 <vitor.xlr8@gmail.com>
2024-02-06 15:51:20 -0400

    fix: update import paths in page.tsx files

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,10 +1,10 @@
 import { db } from '@/app/_lib/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { NextAuthOptions } from 'next-auth';
+import { AuthOptions } from 'next-auth';
 import { Adapter } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
