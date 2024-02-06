b35e5fb6c732d6684abd49bedce09f13162f7a6b
VitorLima1998 <vitor.xlr8@gmail.com>
2024-02-06 15:46:24 -0400

    fix: update NextAuth import and type in auth.ts and route.ts

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,10 +1,10 @@
 import { db } from '@/app/_lib/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { AuthOptions } from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
 import { Adapter } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
@@ -23,5 +23,5 @@ export const authOptions: AuthOptions = {
       return session;
     },
   },
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXT_AUTH_SECRET as string,
 };
